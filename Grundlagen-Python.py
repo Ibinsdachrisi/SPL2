63feccd2092e30ad0db3a216f07f5a81d568cb62
Ibinsdachrisi <171wita31@o365.htl-leoben.at>
2018-04-23 15:22:34 +0200

    PC/-User Game

--- Grundlagen-Python.py
+++ Grundlagen-Python.py
@@ -1,5 +1,5 @@
 # grundlagen python.py
-
+import random
 # <-- Kommentare
 
 # Ausgabe von daten
@@ -23,3 +23,27 @@ else:
 lieblingszahl = input("Was ist deine Lieblingszahl? ")
 print("Super, ich mag die Zahl " , lieblingszahl)
 print("Aber die groessere Zahl", int(lieblingszahl)+10, " mag ich noch mehr!")
+
+runden = input("Wie viele Runden sollen wir spielen? ")
+runden = int(runden)
+
+user = 0
+Pc = 0
+for i in range(0,runden):
+    sieger = " "
+    #zufallszahl erzeugen
+    zufallszahl = random.randint(1,6)
+    if (zufallszahl == 1 or zufallszahl == 3 or zufallszahl == 5):
+        sieger = "ich"
+        user = user + 1
+    else:
+        sieger = "Pc"
+        Pc =  Pc + 1
+    
+    print("Runde " , (i + 1) , " von " , runden , ": Sieger: " , sieger , " <" , zufallszahl , ">")
+if(user > Pc):
+    print("Es hat <User> gewonnen ( mit " , user , " Siegen zu " , Pc , " )")
+elif(user < Pc):
+    print("Es hat <Pc> gewonnen ( mit " , Pc , " Siegen zu " , user , " )")
+else:
+    print("Unentschieden ( mit "  , user , " Siegen zu " , Pc , " )")
