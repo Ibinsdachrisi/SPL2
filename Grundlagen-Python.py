c80e920b32791cc2c904e831adb81d477ef14642
Ibinsdachrisi <171wita31@o365.htl-leoben.at>
2018-04-23 14:21:32 +0200

    Content added

--- Grundlagen-Python.py
+++ Grundlagen-Python.py
@@ -1,1 +1,18 @@
 # grundlagen python.py
+
+# <-- Kommentare
+
+# Ausgabe von daten
+print("Hello World")
+
+# Variablen definieren (kann ohne Typ erfolgen)
+heimat = "Erde"
+
+# Mehrere Variablen werden mit , getrennt
+print(heimat, "to World: " , "Hallo!")
+
+# Eingabe / liest Text(!) von der Konsole ein
+wer = input("Und wer bist du? ")
+
+# und gibt den Text wieder aus
+print("Hallo " , wer)
